50007147731ed16774d76c524befd28dbb120fa9
RachmanForniandi <rachmanforniandi@gmail.com>
2022-12-31 19:09:11 +0700

    added & checked permission access image gallery done.

--- app/src/main/java/rachman/forniandi/gatherplacesapp/AddPlaceActivity.kt
+++ app/src/main/java/rachman/forniandi/gatherplacesapp/AddPlaceActivity.kt
@@ -1,9 +1,26 @@
+@file:Suppress("DEPRECATION")
+
 package rachman.forniandi.gatherplacesapp
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.AlertDialogLayout
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.DexterBuilder.MultiPermissionListener
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import rachman.forniandi.gatherplacesapp.databinding.ActivityAddPlaceBinding
 import rachman.forniandi.gatherplacesapp.databinding.ActivityMainBinding
 import java.text.SimpleDateFormat
@@ -39,7 +56,7 @@ class AddPlaceActivity : AppCompatActivity(),View.OnClickListener {
 
         updateDateInView()
         binding?.etDate?.setOnClickListener(this)
-
+        binding?.txtAddImg?.setOnClickListener(this)
 
     }
 
@@ -52,6 +69,20 @@ class AddPlaceActivity : AppCompatActivity(),View.OnClickListener {
                 calendar.get(Calendar.DAY_OF_MONTH))
                     .show()
             }
+            R.id.txt_add_img->{
+                val pictureDialog= AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+                val pictureDialogOptions = arrayOf("Select photo from gallery",
+                    "Select photo from camera")
+                pictureDialog.setItems(pictureDialogOptions){
+                    dialog,options->
+                    when(options){
+                        0-> choosePhotoFromGallery()
+                        1-> choosePhotoFromCamera()
+                    }
+                }
+                pictureDialog.show()
+            }
         }
     }
 
@@ -59,5 +90,67 @@ class AddPlaceActivity : AppCompatActivity(),View.OnClickListener {
         val myFormat = "dd.MM.yyyy"
         val sdf = SimpleDateFormat(myFormat,Locale.getDefault())
         binding?.etDate?.setText(sdf.format(calendar.time).toString())
+    }
+
+    private fun choosePhotoFromGallery(){
+        Dexter.withActivity(this).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            .withListener(object:MultiplePermissionsListener{
+                override fun onPermissionsChecked(
+                    report: MultiplePermissionsReport?) {
+                    if (report != null) {
+                        if (report.areAllPermissionsGranted()){
+                            Toast
+                                .makeText(this@AddPlaceActivity,"Storage Read/Write Permission granted. Now you can select an image from storage.",Toast.LENGTH_SHORT).show()
+                            /*val galleryIntent = Intent(
+                                Intent.ACTION_PICK,
+                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                            )
+                            startActivityForResult(galleryIntent, CAMERA)*/
+                        }
+
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+
+            }).onSameThread().check()
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. " +
+                    "It can be enabled under Application Settings")
+            .setPositiveButton("GO TO SETTINGS"){_,_->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package",packageName,null)
+                    intent.data = uri
+                    startActivity(intent)
+                }catch (e:ActivityNotFoundException){
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel"){dialog,_->
+                dialog.dismiss()
+            }.show()
+    }
+
+    private fun choosePhotoFromCamera(){
+
+    }
+
+    companion object{
+        private const val GALLERY = 1
+        private const val CAMERA = 2
+        private const val IMAGE_DIRECTORY = "CollectionPlacesImages"
+        // A constant variable for place picker
+        private const val PLACE_AUTOCOMPLETE_REQUEST_CODE = 3
     }
 }
